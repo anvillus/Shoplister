6b4b211c2ae09d6881f87f9795c8f5555c55ca4e
Sullivan Lacheteau <sullivan.lacheteau@outlook.com>
2026-05-11 21:32:03 -0400

    Added file-scoped namespaces

--- Shoplister.Mobile/App.xaml.cs
+++ Shoplister.Mobile/App.xaml.cs
@@ -1,15 +1,14 @@
-﻿namespace Shoplister.Mobile
-{
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
-        }
+﻿namespace Shoplister.Mobile;
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
-        }
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new AppShell());
     }
 }
